c69c5d698c4bbb146ae1cf5f8377e36ea0c74ada
Salvatore Torcivia <github+0019@saltor.nyc>
2022-01-23 17:16:40 -0500

    Hide the secret number duhhh

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,8 +7,6 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("The secret number is: {}", secret_number);
-
     loop {
         println!("Please input your guess.");
 
@@ -20,7 +18,10 @@ fn main() {
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                println!("Please enter a number!");
+                continue;
+            },
         };
 
         println!("You guessed: {}", guess);
