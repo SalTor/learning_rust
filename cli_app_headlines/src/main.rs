955458fcbbb668ca1fdf54eeee81f85347f0d63a
Salvatore Torcivia <github+0019@saltor.nyc>
2022-05-16 22:47:37 -0400

    Scrape hackernews for front page titles!

--- cli_app_headlines/src/main.rs
+++ cli_app_headlines/src/main.rs
@@ -1,11 +1,28 @@
 extern crate reqwest;
 extern crate select;
+extern crate scraper;
 
 use select::document::Document;
 use select::predicate::Name;
+use scraper::{Html, Selector};
 
 fn main() {
-    scrape("https://saltor.nyc")
+    scrape("https://saltor.nyc");
+    hn_headlines("https://news.ycombinator.com")
+}
+
+fn hn_headlines(url: &str) {
+    let mut resp = reqwest::get(url).unwrap();
+    assert!(resp.status().is_success());
+
+    let body = resp.text().unwrap();
+    let fragment = Html::parse_document(&body);
+    let stories = Selector::parse(".titlelink").unwrap();
+
+    for story in fragment.select(&stories) {
+        let story_txt = story.text().collect::<Vec<_>>();
+        println!("{:?}", story_txt);
+    }
 }
 
 fn scrape(url: &str) {
